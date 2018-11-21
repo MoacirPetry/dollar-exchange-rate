1b5279a597e42e2543b0c79949a3a99e94599e27
Moacir Petry <m.petry9@gmail.com>
2018-11-21 20:40:19 -0200

    configured api - getting values

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,9 @@
+require 'net/http'
+
 class HomepageController < ApplicationController
   def index
+    dollar_response = Net::HTTP.get_response(URI.parse('https://api.bcb.gov.br/dados/serie/bcdata.sgs.10813/dados/ultimos/1?formato=json'))
+    data = dollar_response.body
+    @result = JSON.parse(data)
   end
 end
