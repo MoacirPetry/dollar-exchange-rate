976c984e3f1880e2bbca9589c22b150b21c04e1d
Moacir Petry <m.petry9@gmail.com>
2018-11-22 00:05:09 -0200

    implemented bulma css and designed the layout

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,7 +2,7 @@ require 'net/http'
 
 class HomepageController < ApplicationController
   def index
-    dollar_response = Net::HTTP.get_response(URI.parse('https://api.bcb.gov.br/dados/serie/bcdata.sgs.10813/dados/ultimos/1?formato=json'))
+    dollar_response = Net::HTTP.get_response(URI.parse('https://api.bcb.gov.br/dados/serie/bcdata.sgs.10813/dados/ultimos/3?formato=json'))
     data = dollar_response.body
     @result = JSON.parse(data)
   end
