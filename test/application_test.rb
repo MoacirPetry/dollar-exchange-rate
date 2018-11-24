37a58f0400b004c5f86c636fed0e6e6505cd703f
Moacir Petry <m.petry9@gmail.com>
2018-11-23 22:56:23 -0200

    created api test

--- test/application_test.rb
+++ test/application_test.rb
@@ -1,17 +1,18 @@
+require 'net/http'
 require 'test_helper'
 #require 'rails-controller-testing'
 
 class HomepageControllerTest < ActionDispatch::IntegrationTest
 
   test 'controller - getting index' do
-    get '/homepage/index'
+    get root_path
     assert_response :success
     assert_template :index
     assert_template layout: 'layouts/application'
   end
 
   test 'view - getting data' do
-    get '/homepage/index'
+    get root_path
     assert_select 'title', 'Dollar Exchange Rate'
     assert_select 'strong', '1 USD - Last 3 days'
     assert_select 'strong', 'BRL'
@@ -23,10 +24,18 @@ class HomepageControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'http content' do
-    get '/homepage/index'
+    get root_path
     assert_match 'utf-8', @response.body
     assert_match 'width=device-width, initial-scale=1', @response.body
     assert_equal "text/html", @response.content_type
+  end
+
+  test 'api' do
+    api = Net::HTTP.get_response(URI.parse('https://api.bcb.gov.br/dados/serie/bcdata.sgs.10813/dados/ultimos/3?formato=json'))
+    assert_match api.code, '200'
+    assert api.body.present?
+    api_json = JSON.parse(api.body)
+    assert api_json.present?
   end
 
 end
