f499877fe61ec1afad6590bc9d901acdb142787e
Moacir Petry <m.petry9@gmail.com>
2018-11-23 22:28:45 -0200

    routes test added

--- test/application_test.rb
+++ test/application_test.rb
@@ -10,4 +10,9 @@ class HomepageControllerTest < ActionDispatch::IntegrationTest
     assert_template layout: 'layouts/application'
   end
 
+  test 'routes' do
+    assert_routing root_path, controller: 'homepage', action: 'index'
+    assert_recognizes({controller: 'homepage', action: 'index'}, {path: 'homepage/index', method: :get})
+  end
+
 end
