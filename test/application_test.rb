cf790f4139a4ca1ec8bc5cd613fe59e479362831
Moacir Petry <m.petry9@gmail.com>
2018-11-23 22:33:46 -0200

    created http test

--- test/application_test.rb
+++ test/application_test.rb
@@ -15,10 +15,18 @@ class HomepageControllerTest < ActionDispatch::IntegrationTest
     assert_select 'title', 'Dollar Exchange Rate'
     assert_select 'strong', '1 USD - Last 3 days'
     assert_select 'strong', 'BRL'
+  end
 
   test 'routes' do
     assert_routing root_path, controller: 'homepage', action: 'index'
     assert_recognizes({controller: 'homepage', action: 'index'}, {path: 'homepage/index', method: :get})
+  end
+
+  test 'http content' do
+    get '/homepage/index'
+    assert_match 'utf-8', @response.body
+    assert_match 'width=device-width, initial-scale=1', @response.body
+    assert_equal "text/html", @response.content_type
   end
 
 end
